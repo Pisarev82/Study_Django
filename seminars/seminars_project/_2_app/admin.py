e4b3682d4ecc94808d69cfe49845602308bceffa
Pisarev <pisarev82@yandex.ru>
2023-09-15 13:59:02 +0300

    Решение домашнего задания к семинару 5

--- seminars/seminars_project/_2_app/admin.py
+++ seminars/seminars_project/_2_app/admin.py
@@ -1,3 +1,51 @@
 from django.contrib import admin
+from .models import Trow, Author, Article, Comment
 
-# Register your models here.
+
+
+
+@admin.action(description='Сбросить сортировку')
+def reset(modeladmin, request, queryset):
+    queryset.update(quantity=0)
+
+
+class AutorAdmin(admin.ModelAdmin):
+    list_display = ['name', 'surname', 'email', 'biography', 'birthday']
+    list_filter = ['name', 'surname']
+    search_fields = ['name']
+    actions = ['reset']
+
+
+class ArticleAdmin(admin.ModelAdmin):
+    # list_display = ['title', 'content', 'author', ]
+    ordering = ['publication_date']
+    search_fields = ['author']
+    actions = ['reset']
+
+    fieldsets = [
+        (
+            None,
+            {
+                'classes': ['wide'],
+                'fields': ['title'],
+            }
+        ),
+        (
+            'Подробности',
+            {
+                'classes': ['collapse'],
+                'description': 'Полный текст статьи и автор',
+                'fields': ['content', 'author'],
+            }
+        )
+    ]
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Author, AutorAdmin)
+admin.site.register(Article, ArticleAdmin)
+
--- seminars/seminars_project/online_store/admin.py
+++ seminars/seminars_project/online_store/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from .models import Client, Product, Order, OrderItem
 
-# Register your models here.
+
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'phone_number', 'address', 'date_registered')
+    list_filter = ('date_registered',)
+    search_fields = ('name', 'email')
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'price', 'quantity', 'added_date')
+    list_filter = ('added_date',)
+    search_fields = ('name', 'description')
+    list_editable = ('price', 'quantity')  # Редактируемая цена и количество на списке
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('customer', 'total_price', 'date_ordered')
+    list_filter = ('date_ordered',)
+    search_fields = ('customer__name',)
+    filter_horizontal = ('products',)  # Улучшенный вид для выбора продуктов
+
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ('order', 'product', 'price', 'quantity')
+    list_filter = ('order__date_ordered',)
+    search_fields = ('product__name', 'order__customer__name')
